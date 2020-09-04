52f24c1374510ac5eefdcd5f928140feefa81e99
vitalie.jicol <63916548+vitaliejicol@users.noreply.github.com>
2020-09-04 13:53:57 -0500

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -20,7 +20,7 @@ if [ ! -f "$DATAFILE" ]; then
     return 1
 fi
 # Get env from DATAFILE
-ENVIRONMENT=$(sed -nr 's/^\s*environment\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
+ENVIRONMENT=$(sed -nr 's/^\s*env.BRANCH_NAME\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
 S3BUCKET=$(sed -nr 's/^\s*s3_bucket\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
 S3BUCKETPROJ=$(sed -nr 's/^\s*s3_folder_project\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
 S3BUCKETREGION=$(sed -nr 's/^\s*s3_folder_region\s*=\s*"([^"]*)".*$/\1/p' "$DATAFILE")
